3833e7c0f08e26a0b94d431514583e8fa5a70ac2
fendyk <raul.fernandezvandijk@gmail.com>
2022-05-30 15:47:08 +0200

    Add more stuff

--- resources/js/Pages/calendar/Create.vue
+++ resources/js/Pages/calendar/Create.vue
@@ -1,10 +1,10 @@
 <script setup>
 import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
-import { Head, Link } from '@inertiajs/inertia-vue3';
+import { Head } from '@inertiajs/inertia-vue3';
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head title="Calendar - Create" />
 
     <BreezeAuthenticatedLayout>
         <template #header>
@@ -14,26 +14,51 @@ import { Head, Link } from '@inertiajs/inertia-vue3';
         </template>
 
         <div class="mx-auto max-w-7xl p-8">
-            <div class="bg-white p-8 shadow-2xl shadow-gray-200 rounded-3xl">
-                <div class="mb-4 flex flex-row justify-end bg-white">
-
-                    <Link :href="route('calendar/Create')" class="bg-blue-600 text-white rounded-3xl px-2 py-1">
-                        Voeg nieuw item toe
-                    </Link>
+            <div class="bg-white p-8 rounded-3xl shadow-2xl shadow-slate-200">
+                <div class="flex flex-row justify-between items-center">
+                    <div>
+                        <h2 class="text-2xl font-bold">Voeg makkelijk nieuwe items toe</h2>
+                    </div>
+                    <v-calendar :attributes='attrs'></v-calendar>
+                    <div class="flex flex-col space-y-4">
+                        <div class="flex flex-col">
+                            <label class="text-gray-400">Titel</label>
+                            <input type="text" placeholder="Titel invoeren.." class="rounded-3xl border-0 bg-gray-100" />
+                        </div>
+                        <div class="flex flex-col">
+                            <label class="text-gray-400">Tag</label>
+                            <input type="text" placeholder="Voer een tag in.." class="rounded-3xl border-0 bg-gray-100" />
+                        </div>
+                        <div class="flex flex-col">
+                            <label class="text-gray-400">Tijd - van / tot</label>
+                            <div class="flex flex-row">
+                                <input type="time" class="rounded-3xl border-0 bg-gray-100" />
+                                <input type="time" class="rounded-3xl border-0 bg-gray-100" />
+                            </div>
+                        </div>
+                        <div class="flex flex-col">
+                            <label class="text-gray-400">Selecteer gebruikers</label>
+                            <select class="rounded-3xl border-0 bg-gray-100">
+                                <option></option>
+                            </select>
+                        </div>
+                        <div class="flex flex-col">
+                            <input type="submit" value="Opslaan" class="rounded-3xl border-0 bg-blue-500 text-white py-2 font-bold" />
+                        </div>
+                    </div>
                 </div>
-                <v-calendar is-expanded :attributes='attrs'></v-calendar>
             </div>
         </div>
 
         <div class="mx-auto max-w-7xl p-8">
-            <div class="bg-white p-8 rounded-3xl shadow-2xl shadow-gray-200 grid space-y-4">
+            <div class="bg-white p-8 rounded-3xl shadow-2xl shadow-slate-200">
 
                 <div>
-                    <h2 class="text-2xl font-bold text-gray-900">31 Mei 2022</h2>
+                    <h2 class="text-2xl font-bold">5 mei 2022</h2>
                     <p class="text-purple-500">Opkomende eventen</p>
                 </div>
 
-                <div class="grid space-y-4">
+                <div class="grid space-y-4 mt-4">
                     <div class="border-b-2 py-4 border-gray-100 grid grid-flow-col items-center justify-between">
                         <div>
                             <h3 class="text-gray-800 text-xl font-bold">Tweede kamer debat in de plenaire zaal</h3>
--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/calendar/create', function () {
+    return Inertia::render('calendar/Create');
+})->middleware(['auth', 'verified'])->name('calendar/Create');
+
 require __DIR__.'/auth.php';
